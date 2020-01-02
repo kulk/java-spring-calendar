b0eed5cb1dc20f9da7345b83ed8cec5db56a59f8
Roeland Kulk <roelandkulk@gmail.com>
2020-01-02 22:14:00 +0100

    map iteration works, no if condition yet

--- src/main/java/spring/calendar/controller/CalendarController.java
+++ src/main/java/spring/calendar/controller/CalendarController.java
@@ -10,6 +10,7 @@ import spring.calendar.model.User;
 import spring.calendar.model.service.LoginService;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
@@ -23,6 +24,29 @@ public class CalendarController {
     public String calendarLauncher(@SessionAttribute("user") User user, Model model){
         List<Event> events = user.getEvents();
         model.addAttribute("events", events);
+        ArrayList<Event> testEvents = new ArrayList<>();
+        testEvents.add(events.get(0));
+        testEvents.add(events.get(1));
+        testEvents.add(events.get(2));
+        testEvents.add(events.get(3));
+        HashMap<String, ArrayList<Event>> eventMap = createEventMap(testEvents);
+        model.addAttribute("testEvents", eventMap);
         return "calendar";
+    }
+
+    public HashMap<String, ArrayList<Event>> createEventMap(ArrayList<Event> events){
+        HashMap<String, ArrayList<Event>> eventMap = new HashMap<>();
+        ArrayList<Event> events1 = new ArrayList<>();
+        ArrayList<Event> events2 = new ArrayList<>();
+        for (int i = 0; i < events.size() ; i++) {
+            if(i % 1 == 0){
+                events1.add(events.get(i));
+            } else {
+                events2.add(events.get(i));
+            }
+        }
+        eventMap.put("01", events1);
+        eventMap.put("02", events2);
+        return  eventMap;
     }
 }
