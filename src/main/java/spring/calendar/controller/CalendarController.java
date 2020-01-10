4e4024048d503cd0e7c28d5aee9d9496295bc0f2
Roeland Kulk <roelandkulk@gmail.com>
2020-01-10 22:48:14 +0100

    refactor CalendarController

--- src/main/java/spring/calendar/controller/CalendarController.java
+++ src/main/java/spring/calendar/controller/CalendarController.java
@@ -7,63 +7,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import spring.calendar.model.Event;
 import spring.calendar.model.User;
-import spring.calendar.model.service.LoginService;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.time.format.DateTimeFormatter;
-import java.time.format.FormatStyle;
+import spring.calendar.model.service.EventService;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+
 
 @Controller
 public class CalendarController {
 
     @Autowired
-    LoginService loginService;
-
+    EventService eventService;
 
     @GetMapping("calendar")
     public String calendarLauncher(@SessionAttribute("user") User user, Model model){
-        List<Event> events = user.getEvents();
-
-        HashMap<String, ArrayList<Event>> eventMap = createEventMap(events);
+        HashMap<String, ArrayList<Event>> eventMap = eventService.createEventMap(user);
         model.addAttribute("testEvents", eventMap);
-
         return "calendar";
     }
 
-    public HashMap<String, ArrayList<Event>> createEventMap(List<Event> events){
-        HashMap<String, ArrayList<Event>> eventMap = new HashMap<>();
-        String month = "";
-//        * - Loop trough events List
-        for(Event event : events){
-//        * - Look up month of event
-            month = extractKey(event);
-            if(eventMap.containsKey(month)){
-                ArrayList<Event> eventList = eventMap.get(month);
-                eventList.add(event);
-                eventMap.put(month, eventList);
-            } else {
-                ArrayList<Event> eventList = new ArrayList<>();
-                eventList.add(event);
-                eventMap.put(month, eventList);
-            }
-        }
-//        * - Create key String if needed
-//        * - Create value List if needed
-//        * - Add Event to the value List
-        return  eventMap;
-    }
-    /*public ArrayList<Event> addEventToList(ArrayList<Event> eventList){
 
-    }*/
-
-    public String extractKey(Event event){
-        LocalDate date = event.getDate();
-        String stringDate = date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-        String month = String.valueOf(stringDate.charAt(3)) + stringDate.charAt(4);
-        return month;
-    }
 }
--- src/main/java/spring/calendar/model/service/EventService.java
+++ src/main/java/spring/calendar/model/service/EventService.java
@@ -4,6 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import spring.calendar.model.Dao.EventDao;
 import spring.calendar.model.Event;
+import spring.calendar.model.User;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 @Service
 public class EventService {
@@ -14,4 +21,34 @@ public class EventService {
     public void save(Event event){
         eventDao.save(event);
     }
+
+    public HashMap<String, ArrayList<Event>> createEventMap(User user){
+        List<Event> events = user.getEvents();
+        HashMap<String, ArrayList<Event>> eventMap = new HashMap<>();
+        for(Event event : events){
+            eventMap = fillEventMap(eventMap, event);
+        }
+        return  eventMap;
+    }
+
+    private HashMap<String, ArrayList<Event>> fillEventMap(HashMap<String, ArrayList<Event>> eventMap, Event event){
+        String month = extractKey(event);
+        if(eventMap.containsKey(month)){
+            ArrayList<Event> eventList = eventMap.get(month);
+            eventList.add(event);
+            eventMap.put(month, eventList);
+        } else {
+            ArrayList<Event> eventList = new ArrayList<>();
+            eventList.add(event);
+            eventMap.put(month, eventList);
+        }
+        return eventMap;
+    }
+
+    private String extractKey(Event event){
+        LocalDate date = event.getDate();
+        String stringDate = date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        return String.valueOf(stringDate.charAt(3)) + stringDate.charAt(4);
+    }
+    
 }
