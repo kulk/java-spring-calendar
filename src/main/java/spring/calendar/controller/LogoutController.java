7b3fd193a537a50e9f5c3cfa5ebdf19b82250268
Roeland Kulk <roelandkulk@gmail.com>
2020-01-22 16:59:52 +0100

    working on logout function

---
+++ src/main/java/spring/calendar/controller/LogoutController.java
@@ -0,0 +1,21 @@
+package spring.calendar.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import spring.calendar.model.User;
+
+@Controller
+@SessionAttributes("user")
+public class LogoutController {
+
+
+    @GetMapping("logout")
+    public String logoutHandler(@SessionAttribute(required=false, name="user") User user){
+        if(user == null){
+            return "login";
+        }
+        return "login";
+    }
+}
