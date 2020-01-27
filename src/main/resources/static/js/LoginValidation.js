78203e23bb73811393fcdec34012cf79cf96c4b4
Roeland Kulk <roelandkulk@gmail.com>
2020-01-27 11:10:29 +0100

    applied JQuery

--- src/main/resources/static/js/LoginValidation.js
+++ src/main/resources/static/js/LoginValidation.js
@@ -1,19 +1,17 @@
 
-
 function validationError(event) {
-
-    var form_tag_list = [document.login_form.username.value, document.login_form.password.value];
+    var form_tag_list = [$('#id_username').val(), $('#id_password').val()];
     var error_elements = ["userNameError", "passwordError" ];
     var error_strings = ["E-mail", "Password"];
 
     //Haal error berichten weg voor het geval er nog errors berichten stonden
     for (let i = 0; i < error_elements.length; i++) {
-        document.getElementById(error_elements[i]).innerHTML = " ";
+        $("#" + error_elements[i]).html(" ");
     }
     //Geef error voor velden die leeg zijn
     for (let i = 0; i < form_tag_list.length; i++) {
         if(form_tag_list[i] === ""){
-            document.getElementById(error_elements[i]).innerHTML = error_strings[i] + " field cannot be empty";
+            $("#" + error_elements[i]).html(error_strings[i] + " field cannot be empty");
             event.preventDefault();
         }
     }
